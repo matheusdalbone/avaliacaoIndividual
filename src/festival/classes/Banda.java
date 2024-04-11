95d4f892e85ea4878436c7be8cb16bc313c5cb24
Matheus Dalbone <matheusdalbone7@gmail.com>
2024-04-10 21:10:30 -0300

    delete tipo from banda/cantor

--- src/festival/classes/Banda.java
+++ src/festival/classes/Banda.java
@@ -1,14 +1,8 @@
 package festival.classes;
 
 public class Banda extends Artista{
-	protected static String tipo = "Banda";
-		
 	public Banda(String nome, double cache) {
 		super(nome, cache);
-	}
-
-	public static String getTipo() {
-		return tipo;
 	}
 
 }
--- src/festival/classes/Cantor.java
+++ src/festival/classes/Cantor.java
@@ -2,15 +2,7 @@ package festival.classes;
 
 public class Cantor extends Artista{
 	
-	protected static String tipo = "Cantor";
-	
 	public Cantor(String nome, double cache) {
 		super(nome, cache);
 	}
-
-	public static String getTipo() {
-		return tipo;
-	}
-	
-
 }
